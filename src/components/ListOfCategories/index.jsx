26a69a2e4e6936b814a0118c752f013fb44d8f88
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-09-19 12:57:33 -0500

    List of Categories component updated

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -2,15 +2,34 @@ import React from "react";
 import { Category } from "@/components/Category";
 import { List, Item } from "./styles";
 
-import { categories } from "@/api/db.json"
+import { categories as mockCategories } from "@/api/db.json"
 
 
 export const ListOfCategories = () => {
-    return(
-        <List>
-            {
-                categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-            }
+    const [categories, setCategories] = React.useState(mockCategories);
+    const [showFixed, setShowFixed] = React.useState(false);
+
+    const renderList = (fixed) => (
+        <List fixed={fixed}>
+        {
+            categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        }
         </List>
+    )
+
+    React.useEffect(() => {
+        const onScroll = (e) => {
+            const newShowFixed = window.scrollY > 200;
+            newShowFixed !== showFixed && setShowFixed(newShowFixed);
+        }
+        document.addEventListener('scroll', onScroll)
+        return () => document.removeEventListener('scroll', onScroll)
+    })
+
+    return(
+        <>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </>
     )
 }
