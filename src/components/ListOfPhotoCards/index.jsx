540fbffc23f657d018f76ca46caa89ec22d9f1cd
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-09-20 23:07:55 -0500

    Added some hooks

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -5,7 +5,7 @@ import { PhotoCard } from "@/components/PhotoCard";
 export const ListOfPhotoCards = () => {
     return(
         <ul>
-            {[1, 2, 3, 4, 5].map(id => <PhotoCard/>)}
+            {[1, 2, 3, 4, 5].map(id => <PhotoCard key={id} id={id}/>)}
         </ul>
     )
 }
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,45 +1,37 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { MdFavoriteBorder } from "react-icons/md";
+import { useLocalStorage } from "@/hooks/useLocalStorage"
+import { useNearScreen } from "@/hooks/useNearScreen";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import { ImgWrapper, Img, Button, Article } from "./styles";
+
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1508280756091-9bdd7ef1f463?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'; 
 
-export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE}) => {
-    const [show, setShow] = useState(false);
-    const element = useRef(null);
 
-    useEffect(() => {
-        Promise.resolve(
-            typeof window.IntersectionObserver !== 'undefined'
-            ? window.IntersectionObserver
-            :import('intersection-observer')
-        )      
-        .then(() => {
-            const observer = new window.IntersectionObserver((entries) => {
-                const { isIntersecting } = entries[0];
-                console.log("intersecting: ", isIntersecting);
-                if (isIntersecting) {
-                    setShow(true);
-                    observer.disconnect();
-                }
-            })
-            observer.observe(element.current);
-        })
-    }, [element]);
+export const PhotoCard = ({ id = 1, likes = 0, src=DEFAULT_IMAGE}) => {
+    const [show, element] = useNearScreen();
+    const key = `like-${id}`;
+    const [liked, setLiked] = useLocalStorage(key, false);
+    
+
+
+    const Icon = liked ? MdFavorite : MdFavoriteBorder;
 
     return(
         <Article ref={element}>
-            {show &&      
+            {show && 
+            <>   
             <a href={`/detail/${id}`}>
                 <ImgWrapper>
                     <Img
                         src={src}
                     />
                 </ImgWrapper>
-                <Button>
-                   <MdFavoriteBorder size='32px' /> {likes} likes!
-                </Button>
-            </a>            
+            </a>    
+            <Button onClick={() =>{setLiked(!liked)}}>
+                <Icon size='32px' /> {likes} likes!
+            </Button>   
+            </>        
             }
         </Article>
     )
